9236033503d87d017895e3ef1dd23c2aa74a817d
Ermis <cydisalt@gmail.com>
2022-12-30 01:50:25 +0200

    bio

--- pages/[locale]/bio.tsx
+++ pages/[locale]/bio.tsx
@@ -35,25 +35,6 @@ const ContactPage = observer(() => {
     >
       Biography
     </Typography>
-    {/* <Typography paragraph>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lobortis viverra lacinia. Aenean blandit enim ex, ac porttitor ante accumsan nec. Nunc vulputate leo leo. Morbi dignissim ac quam sed condimentum. Sed imperdiet felis leo, vitae efficitur libero finibus vel. Sed fermentum, nisl vel venenatis egestas, libero enim pulvinar ligula, eu pellentesque purus ex sed nisi. Donec metus libero, volutpat vel fringilla eu, sodales quis augue.
-    </Typography>
-
-    <Typography paragraph>
-      In aliquam nec nisl nec semper. Etiam ut dignissim ante. Ut pulvinar consequat ex, et tempor libero fringilla eu. Praesent sagittis tortor sit amet eros dapibus facilisis. Nam id ligula nisl. Donec suscipit erat vitae porttitor porta. Ut nec arcu ut orci eleifend tristique commodo id turpis. Aenean a dolor sit amet dui tincidunt rhoncus id eu leo. Donec feugiat mi nisl, vitae fermentum ex tristique eu. Nullam aliquet erat id lorem aliquet cursus. Aenean nec lobortis felis. In fringilla ligula ac dui semper, et pretium quam varius. Pellentesque a maximus ipsum. Aenean sed facilisis leo. Sed eleifend erat leo, at feugiat velit elementum at. Nam lobortis pellentesque felis vestibulum vestibulum.
-    </Typography>
-
-    <Typography paragraph>
-      Donec lacinia eget nunc a feugiat. Phasellus sed enim non leo porttitor feugiat pharetra non turpis. Etiam eget metus at diam tristique eleifend eget sit amet ante. Ut blandit, quam non sollicitudin ultricies, tellus erat rutrum orci, ac porta justo tortor semper erat. Morbi est velit, molestie viverra facilisis non, ultrices sit amet lorem. Aliquam sit amet bibendum lorem. Aenean sollicitudin erat porta, hendrerit risus quis, viverra justo. Donec facilisis porta libero, a commodo leo aliquet vel. Suspendisse dictum erat libero, sit amet tempor lacus faucibus vel. In eget neque vulputate, placerat neque quis, tincidunt felis. Nunc quis purus sit amet quam consectetur convallis. Morbi suscipit lobortis odio, at tempus libero. Quisque gravida vulputate cursus. Ut sit amet urna vel massa pellentesque facilisis. Pellentesque ac nunc convallis, lobortis augue sed, laoreet nibh. Integer pretium suscipit nibh in aliquet.
-    </Typography>
-
-    <Typography paragraph>
-      In in dolor a neque volutpat accumsan ac vitae mi. Vivamus elit sem, scelerisque vel diam in, blandit pretium ex. Aenean purus ipsum, sollicitudin ac lobortis sollicitudin, posuere a velit. Sed maximus nulla vitae elit placerat, in sodales orci molestie. Maecenas tincidunt libero quam, sit amet pellentesque leo vestibulum at. Etiam quis pulvinar metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    </Typography>
-
-    <Typography paragraph>
-      Integer cursus dolor non felis ultrices, tincidunt fringilla leo pellentesque. Donec in turpis sodales, feugiat nunc eu, tristique mauris. Nulla ultricies lorem in libero dapibus, vel ultricies turpis blandit. Donec ut urna vehicula, eleifend libero vel, tristique libero. Aliquam vel aliquam tortor. Nunc eget tincidunt arcu. Proin vulputate odio metus, non dapibus mi euismod eu. Suspendisse vestibulum quam eu condimentum pulvinar. Proin magna tortor, tincidunt ut neque non, eleifend semper lorem. Vivamus sagittis viverra commodo.
-    </Typography> */}
 
     <Typography paragraph>
       Apostolos Pagonis (also known as "Pagonini", "Αποστολλων" and "Ο ξανθός απόλυτος γκόμενος με τα μπράτσα") was a renowned personality of the 21st century. Well known as a cellist but better known as a pornstar that wouldn't stop, there is little doubt that he excelled at everything he tried his hand at (except maths, drawing and not-constantly-wanking-off.)
